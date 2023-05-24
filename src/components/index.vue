37efde26357d09a1feddc05684b61bf8e0ecc309
Chenxyan99 <251601918@qq.com>
2023-05-24 10:39:43 +0800

    6

--- src/components/index.vue
+++ src/components/index.vue
@@ -4,6 +4,8 @@
   </div>
 </template>
 <script lang="ts">
+import { drawSatellite } from "../utils/satellite.js";
+
 export default {
   name: "Map",
   data() {
@@ -90,66 +92,66 @@ export default {
           };
           // 卫星绘制
           setTimeout(function () {
-            this.drawSatellite();
+            drawSatellite();
           }, 1000);
         })
         .catch((err) => {
           console.log(err);
         });
     },
-    drawSatellite() {
-      console.log(this.positions.length);
-      const Cesium = this.cesium;
-      var positionProperty = new Cesium.SampledPositionProperty();
-      var TimeInterval = 60;
-      for (var i = 0; i < this.positions.length; i++) {
-        var time = Cesium.JulianDate.addSeconds(
-          this.v.clock.currentTime,
-          i * TimeInterval,
-          new Cesium.JulianDate()
-        );
-        var position = Cesium.Cartesian3.fromDegrees(
-          this.positions[i].lon,
-          this.positions[i].lat,
-          this.positions[i].alt
-        );
-        positionProperty.addSample(time, position);
-      }
+    // drawSatellite() {
+    //   console.log(this.positions.length);
+    //   const Cesium = this.cesium;
+    //   var positionProperty = new Cesium.SampledPositionProperty();
+    //   var TimeInterval = 60;
+    //   for (var i = 0; i < this.positions.length; i++) {
+    //     var time = Cesium.JulianDate.addSeconds(
+    //       this.v.clock.currentTime,
+    //       i * TimeInterval,
+    //       new Cesium.JulianDate()
+    //     );
+    //     var position = Cesium.Cartesian3.fromDegrees(
+    //       this.positions[i].lon,
+    //       this.positions[i].lat,
+    //       this.positions[i].alt
+    //     );
+    //     positionProperty.addSample(time, position);
+    //   }
 
-      var satelliteEntity = new Cesium.Entity({
-        availability: new Cesium.TimeIntervalCollection([
-          new Cesium.TimeInterval({
-            start: this.v.clock.currentTime,
-            stop: Cesium.JulianDate.addSeconds(
-              this.v.clock.currentTime,
-              60 * 60 * 24,
-              new Cesium.JulianDate()
-            ),
-          }),
-        ]),
-        position: positionProperty,
-        orientation: new Cesium.VelocityOrientationProperty(positionProperty),
-        // 卫星模型
-        model: {
-          uri: "./wx3.glb",
-          scale: 300,
-        },
-        path: {
-          resolution: 1,
-          material: Cesium.Color.WHITE,
-          width: 0.5,
-        },
-      });
+    //   var satelliteEntity = new Cesium.Entity({
+    //     availability: new Cesium.TimeIntervalCollection([
+    //       new Cesium.TimeInterval({
+    //         start: this.v.clock.currentTime,
+    //         stop: Cesium.JulianDate.addSeconds(
+    //           this.v.clock.currentTime,
+    //           60 * 60 * 24,
+    //           new Cesium.JulianDate()
+    //         ),
+    //       }),
+    //     ]),
+    //     position: positionProperty,
+    //     orientation: new Cesium.VelocityOrientationProperty(positionProperty),
+    //     // 卫星模型
+    //     model: {
+    //       uri: "./wx3.glb",
+    //       scale: 300,
+    //     },
+    //     path: {
+    //       resolution: 1,
+    //       material: Cesium.Color.WHITE,
+    //       width: 0.5,
+    //     },
+    //   });
 
-      // 插值器
-      satelliteEntity.position.setInterpolationOptions({
-        interpolationDegree: 5,
-        interpolationAlgorithm: Cesium.LagrangePolynomialApproximation,
-      });
+    //   // 插值器
+    //   satelliteEntity.position.setInterpolationOptions({
+    //     interpolationDegree: 5,
+    //     interpolationAlgorithm: Cesium.LagrangePolynomialApproximation,
+    //   });
 
-      // 将卫星实体添加到Cesium的Viewer中显示
-      this.v.entities.add(satelliteEntity);
-    },
+    //   // 将卫星实体添加到Cesium的Viewer中显示
+    //   this.v.entities.add(satelliteEntity);
+    // },
   },
   mounted() {
     // 初始化
